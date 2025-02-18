96b2085dc246d1fdef1d1000984d766f4206ffe0
chris wren <chriswrendeveloper@gmail.com>
2025-02-18 18:22:03 +0000

    Added Sidebar layout markup

--- client/src/app/dashboardWrapper.tsx
+++ client/src/app/dashboardWrapper.tsx
@@ -1,16 +1,17 @@
 "use client";
 
 import NavBar from "@/app/(components)/NavBar";
+import SideBar from "@/app/(components)/SideBar";
 import React from "react";
 
 const DashboardWrapper = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="flex min-h-screen w-full bg-gray-50 text-gray-900">
-    {/* Sidebar */}
+      <SideBar />
       <main
         className={`flex w-full flex-col bg-gray-50 dark:bg-dark-bg md:pl-64`}
       >
-      <NavBar/>
+        <NavBar />
         {children}
       </main>
     </div>
