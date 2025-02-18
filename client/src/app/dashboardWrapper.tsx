9d58434595340da6dfe263d007bb998d807e066c
chris wren <chriswrendeveloper@gmail.com>
2025-02-18 16:41:38 +0000

    Added Dashboard Wrapper and NavBar markup

---
+++ client/src/app/dashboardWrapper.tsx
@@ -0,0 +1,20 @@
+"use client";
+
+import NavBar from "@/app/(components)/NavBar";
+import React from "react";
+
+const DashboardWrapper = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div className="flex min-h-screen w-full bg-gray-50 text-gray-900">
+    {/* Sidebar */}
+      <main
+        className={`flex w-full flex-col bg-gray-50 dark:bg-dark-bg md:pl-64`}
+      >
+      <NavBar/>
+        {children}
+      </main>
+    </div>
+  );
+};
+
+export default DashboardWrapper;
