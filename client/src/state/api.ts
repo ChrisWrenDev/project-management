f83810f4ee82579bf1a7571b0c90960f0b078ce7
chris wren <chriswrendeveloper@gmail.com>
2025-03-01 10:12:16 +0000

    Added Priority Layout

--- client/src/state/api.ts
+++ client/src/state/api.ts
@@ -117,6 +117,13 @@ export const api = createApi({
         { type: "Tasks", id: taskId },
       ],
     }),
+    getTasksByUser: build.query<Task[], number>({
+      query: (userId) => `tasks/user/${userId}`,
+      providesTags: (result, error, userId) =>
+        result
+          ? result.map(({ id }) => ({ type: "Tasks", id }))
+          : [{ type: "Tasks", id: userId }],
+    }),
     getUsers: build.query<User[], void>({
       query: () => "users",
       providesTags: ["Users"],
@@ -140,4 +147,5 @@ export const {
   useSearchQuery,
   useGetUsersQuery,
   useGetTeamsQuery,
+  useGetTasksByUserQuery,
 } = api;
